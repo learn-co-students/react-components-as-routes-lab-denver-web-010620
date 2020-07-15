7442383780dcb097d981a75e19038ed90e48594f
etgreer91 <etgreer91@gmail.com>
2020-07-14 21:49:18 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const showActors = actors.map(actor => {
+    return(
+      <div>
+        <h2>Name: {actor.name}</h2>
+        <h2>Movies</h2>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    );
+  })
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {showActors}
     </div>
   );
 };
